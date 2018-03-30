407d6ed76e1a7ded9e48c093ebf804b189888ce8
Lan Pham <lxazo135@gmail.com>
2018-03-29 21:02:50 -0400

    removed columns

--- client/modules/Inventory/pages/TestInventoryPage/TestInventoryPage.js
+++ client/modules/Inventory/pages/TestInventoryPage/TestInventoryPage.js
@@ -1,7 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import ReactDOM from 'react-dom';
-import Columns from 'react-columns';
 //import { Tabs, Tab } from 'react-bootstrap';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
