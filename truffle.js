63d7d9e0712b305d328a440575ec17f10c3e9d60
= <qdonnelly@temple.edu>
2018-04-02 16:26:47 -0400

    truffle config stuff

--- truffle.js
+++ truffle.js
@@ -1,11 +1,26 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
+  new HDWalletProvider(mnemonic, rpcEndpoint);
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
-    }
-  }
+      network_id: '*',
+    },
+    testrpc: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
+    ganache: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*',
+    },
+  },
 };
