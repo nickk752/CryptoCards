79a4529245c5dd060775ecffe169c030f7700367
nickk752 <nickk752@gmail.com>
2018-04-29 21:42:26 -0400

    re-reworked migration to use promises

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,48 +1,34 @@
-/* const CryptoCards = artifacts.require('CryptoCardsCore');
-const SaleClockAuction = artifacts.require('SaleClockAuction');
-let core;
-module.exports = deployer => {
-  deployer.deploy(CryptoCards).then(() => {
-    // console.log("ADDRESS" + CryptoCards.address);
-    return deployer.deploy(SaleClockAuction, CryptoCards.address, 0);
-  }).then(() => {
-    return CryptoCards.deployed();
-  }).then((instance) => {
-    core = instance;
-    return core.setSaleAuctionAddress(SaleClockAuction.address);
-  }).then((result) => {
-    // console.log("RESULT" + result);
-    return core.unpause();
-  }).then((result) => {
-    // console.log("RESULT3");
-    // console.log( result);
-
-  });
-}; */
-
-
 const CryptoCards = artifacts.require('CryptoCardsCore');
-const SkillScience = artifacts.require('SkillScience');
+const SkillScience = artifacts.require('SkillScienceInterface');
 const SaleClockAuction = artifacts.require('SaleClockAuction');
 
 let core, skill;
 
-module.exports = async deployer => {
+module.exports = deployer => {
 
-  deployer.deploy(CryptoCards).then(await async function deployCryptoCards() {
+  deployer.deploy(CryptoCards).then(() => {
 
     console.log("ADDRESS" + CryptoCards.address);
 
-    const saleInstance = await deployer.deploy(SaleClockAuction, CryptoCards.address, 0);
-    const cryptoInstance = await CryptoCards.deployed();
-
-    await cryptoInstance.setSaleAuctionAddress(saleInstance.address);
-
-    const skillInstance = await deployer.deploy(SkillScience);
-    await cryptoInstance.setSkillScienceAddress(skillInstance.address);
-
-    await cryptoInstance.unpause();
-
-    
+    return deployer.deploy(SaleClockAuction, CryptoCards.address, 0);
+  }).then(() => {
+    CryptoCards.deployed().then((instance) => {
+      core = instance;
+      return core.setSaleAuctionAddress(SaleClockAuction.address);
+    })
+      .then(() => {
+        return deployer.deploy(SkillScience, 8);
+      })
+      .then(() => {
+        SkillScience.deployed()
+          .then((instance) => {
+            skill = instance;
+            return core.setSkillScienceAddress(skill.address);
+          });
+      })      
+      .then(() => {
+        return core.unpause();
+      });
   });
 };
+
